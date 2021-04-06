fe4238474b65c2e671880786b0326f0b91076c54
tim-nnd <timothy.ananda@gmail.com>
2021-04-06 16:35:35 +0700

    Initial Commit

---
+++ Diatur-MC1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diatur-MC1
+//
+//  Created by Timothy Ananda on 06/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
