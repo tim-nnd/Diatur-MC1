c5a1265b674e9069acb5a88481ac8ca77d2a8e94
DoofHeinz <daffasatria.ds@gmail.com>
2021-04-07 17:49:17 +0700

    Start Button

---
+++ Diatur-MC1/TaskDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TaskDetailViewController.swift
+//  Diatur-MC1
+//
+//  Created by Daffa Satria on 07/04/21.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func NavBackBtn(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func StartBtn(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
