382de4226dd40598ead79b0ab51966fcdda43e58
ddhyuliani <ddhyuliani@gmail.com>
2021-04-08 03:24:48 +0700

    New Task Page & Edit

--- Diatur-MC1/SceneDelegate.swift
+++ Diatur-MC1/SceneDelegate.swift
@@ -16,7 +16,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        let newTaskStoryboard = UIStoryboard.init(name: "NewTask", bundle: nil)
+        
+        let newTaskVC = newTaskStoryboard.instantiateViewController(withIdentifier: "newTaskVC") as! NewTaskVC
+        self.window?.rootViewController = newTaskVC
+        self.window?.makeKeyAndVisible()
+        
         guard let _ = (scene as? UIWindowScene) else { return }
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- Diatur-MC1/ViewController.swift
+++ Diatur-MC1/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Diatur-MC1
 //
-//  Created by Timothy Ananda on 06/04/21.
+//  Created by Dian Dinihari on 08/04/21.
 //
 
 import UIKit
@@ -11,10 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("Lohaaaaaaa")
-    }
 
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
