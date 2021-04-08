df6168a68ac3087eddd265151d12f35902b7ce7c
kuliModern <ahmad.azka.kusuma@mail.ugm.ac.id>
2021-04-08 14:21:53 +0700

    Task Detail and Table View Added

---
+++ Diatur-MC1/Task/DetailedTaskTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetailedTaskTableViewCell.swift
+//  Diatur-MC1
+//
+//  Created by Azka Kusuma on 08/04/21.
+//
+
+import UIKit
+
+class DetailedTaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Diatur-MC1/Task/OverviewViewController.swift
+++ Diatur-MC1/Task/OverviewViewController.swift
@@ -13,12 +13,18 @@ class OverviewViewController: UIViewController {
     @IBOutlet weak var ListTask: UITableView!
     @IBOutlet weak var todayListTitle: UILabel!
     
+    var cellID = "listCell"
+    
+    // Data dari Table View Cell
+    var taskName: String = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        ListTask.delegate = self
         // Do any additional setup after loading the
-    
+        
     }
     
     @IBAction func addTask(_ sender: UIButton) {
@@ -41,4 +47,20 @@ class OverviewViewController: UIViewController {
     }
     */
 
+}
+
+extension OverviewViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = ListTask.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        return cell
+    }
+    
+    
 }
