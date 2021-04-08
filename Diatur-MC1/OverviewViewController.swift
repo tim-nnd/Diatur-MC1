47a14a9bf9146bce766a599367164b06257fc815
kuliModern <ahmad.azka.kusuma@mail.ugm.ac.id>
2021-04-08 14:00:29 +0700

    added coding

---
+++ Diatur-MC1/OverviewViewController.swift
@@ -0,0 +1,44 @@
+//
+//  OverviewViewController.swift
+//  Diatur-MC1
+//
+//  Created by Azka Kusuma on 08/04/21.
+//
+
+import UIKit
+
+class OverviewViewController: UIViewController {
+
+    
+    @IBOutlet weak var ListTask: UITableView!
+    @IBOutlet weak var todayListTitle: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the
+    
+    }
+    
+    @IBAction func addTask(_ sender: UIButton) {
+        
+        
+        
+    }
+   
+    @IBAction func settingsButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
