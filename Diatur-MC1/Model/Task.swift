7aa67923ffeb4b1d67a61670c183f2cad9de6326
tim-nnd <65796225+tim-nnd@users.noreply.github.com>
2021-04-09 18:42:48 +0700

    Revert "Azkake"

--- Diatur-MC1/Model/Task.swift
+++ Diatur-MC1/Model/Task.swift
@@ -10,29 +10,15 @@ import Foundation
 class Task {
     var name: String
     var priority: Int
-    var timeElapsed: TimeInterval = 0
+    var timeElapsed: TimeInterval
     var date: Date
     var notes: String
     
-    init(name: String, priority: Int, timeElapsed: TimeInterval, date: Date,  notes: String) {
+    init(name: String, priority: Int, timeElapsed: TimeInterval, date: Date, notes: String) {
         self.name = name
         self.priority = priority
         self.timeElapsed = timeElapsed
         self.date = date
         self.notes = notes
-    }
-    
-    //Init Overview
-    init(name: String, priority: Int) {
-        let dateData = "2021-04-20"
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-      
-       
-        self.name = name
-        self.priority = priority
-        self.timeElapsed = Double(0)
-        self.date =  dateFormatter.date(from: dateData)!
-        self.notes = "notes"
     }
 }
