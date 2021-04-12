8173733722c478bb7d2989b7f17a912e0784b0af
zkake <ahmad.azka.kusuma@mail.ugm.ac.id>
2021-04-12 17:16:10 +0700

    Tab Bar

--- Diatur-MC1/NewTask/NewTaskVC.swift
+++ Diatur-MC1/NewTask/NewTaskVC.swift
@@ -81,6 +81,8 @@ class NewTaskVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource 
         datePicker.date = dateObj!
         navBar.title = "Edit"
     }
+    
+    
     @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
