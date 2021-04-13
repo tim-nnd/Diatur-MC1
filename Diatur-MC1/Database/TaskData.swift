23997d5b961277aee9317367157c4d3854c65510
tim-nnd <timothy.ananda@gmail.com>
2021-04-13 11:09:09 +0700

    Added global database file, table in schedule and summary page

---
+++ Diatur-MC1/Database/TaskData.swift
@@ -0,0 +1,17 @@
+//
+//  TaskData.swift
+//  Diatur-MC1
+//
+//  Created by Timothy Ananda on 12/04/21.
+//
+
+import Foundation
+
+struct TaskDatabase {
+    static var taskArray: [Task] = [
+        Task.init(name: "Ngoding Dulu", priority: 0),
+        Task.init(name: "Ahayy Ngoding", priority: 2),
+        Task.init(name: "Lagi MC1.2 Brooow", priority: 1),
+        Task.init(name: "LAGI UTS NICH", priority: 1)
+    ]
+}
